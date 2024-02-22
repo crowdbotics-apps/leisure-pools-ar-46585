95f0e8b9971c9e5935cd7cb7f7110fa49cf38924
Crowdbotics <team@crowdbotics.com>
2024-02-22 19:01:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "leisure-pools-ar-46585",
+  url: "https://leisure-pools-ar-46585.botics.co",
+  api: "https://leisure-pools-ar-46585.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
